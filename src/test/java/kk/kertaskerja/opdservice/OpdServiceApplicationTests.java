4f5dd2634fb8fac94201d6bc10291aeface5329a
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-05-01 23:19:08 +0700

    config for integration test with keycloak container

--- src/test/java/kk/kertaskerja/opdservice/OpdServiceApplicationTests.java
+++ src/test/java/kk/kertaskerja/opdservice/OpdServiceApplicationTests.java
@@ -1,11 +1,23 @@
 package kk.kertaskerja.opdservice;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
 import kk.kertaskerja.opdservice.domain.Opd;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,10 +25,53 @@ import static org.assertj.core.api.Assertions.assertThat;
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 @ActiveProfiles("integration")
+@Testcontainers
 class OpdServiceApplicationTests {
+    private static KeycloakToken bjornToken;
+    private static KeycloakToken isabelleToken;
 
     @Autowired
     private WebTestClient webTestClient;
+
+    @Container
+    private static final KeycloakContainer keycloakContainer = new KeycloakContainer("quay.io/keycloak/keycloak:19.0")
+            .withRealmImportFile("test-realm-config.json");
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+                () -> keycloakContainer.getAuthServerUrl() + "/realms/Kertaskerja");
+    }
+
+    @BeforeAll
+    static void generateAccessToken() {
+        WebClient webClient = WebClient.builder()
+                .baseUrl(keycloakContainer.getAuthServerUrl() + "/realms/Kertaskerja/protocol/openid-connect/token")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .build();
+
+        isabelleToken = authenticateWith("isabelle", "password", webClient);
+        bjornToken = authenticateWith("bjorn", "password", webClient);
+    }
+
+    private static KeycloakToken authenticateWith(String username, String password, WebClient webClient) {
+        return webClient.post().body(
+                BodyInserters.fromFormData("grant_type", "password")
+                        .with("client_id", "kertaskerja-test")
+                        .with("username", username)
+                        .with("password", password))
+                .retrieve()
+                .bodyToMono(KeycloakToken.class)
+                .block();
+    }
+
+    private record KeycloakToken(String access_token) {
+        @JsonCreator
+        private KeycloakToken(@JsonProperty("access_token") final String access_token) {
+            this.access_token = access_token;
+        }
+    }
+
 
     @Test
     void whenPostRequestThenOpdCreated() {
@@ -24,7 +79,7 @@ class OpdServiceApplicationTests {
 
         webTestClient
                 .post()
-                .uri("/opd")
+                .uri("/opds")
                 .bodyValue(expectedOpd)
                 .exchange()
                 .expectStatus().isCreated()
@@ -41,7 +96,7 @@ class OpdServiceApplicationTests {
     void whenGetOpdNotExistingThenReturnNotFound() {
         webTestClient
                 .get()
-                .uri("/opd/1234")
+                .uri("/opds/1234")
                 .exchange()
                 .expectStatus().isNotFound()
                 .expectBody()
@@ -55,14 +110,14 @@ class OpdServiceApplicationTests {
 
         webTestClient
                 .post()
-                .uri("/opd")
+                .uri("/opds")
                 .bodyValue(opdToCreate)
                 .exchange()
                 .expectStatus().isCreated();
 
         webTestClient
                 .post()
-                .uri("/opd")
+                .uri("/opds")
                 .bodyValue(opdToCreate)
                 .exchange()
                 .expectStatus().isEqualTo(422)
