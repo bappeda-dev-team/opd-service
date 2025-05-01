03b0feb82b3a5285c06cfd033edcfcfcbfcdaccd
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-05-01 12:08:45 +0700

    add jwt role based access control

--- src/main/java/kk/kertaskerja/opdservice/web/OpdController.java
+++ src/main/java/kk/kertaskerja/opdservice/web/OpdController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("opd")
+@RequestMapping("opds")
 public class OpdController {
     private final OpdService opdService;
 
